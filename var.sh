c460a2ba6b664c28500a4a3f443bbc3968337808
sameena-art <abbasisameena@gmail.com>
2025-06-24 00:15:06 +0530

    V1

---
+++ var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to show disk space usage of /var
+
+echo "Disk space usage for /var:"
+echo "--------------------------"
+
+# Show disk usage of the /var mount point in human-readable format
+df -h /var
+
+echo "--------------------------"
+
